0d170d93f69ae06a02e289b53c6136121108318b
Sage King <sagekingssk@gmail.com>
2022-05-11 16:29:30 -0600

    InputCommon: Implement a control stick gate for mapping the mouse cursor to a stick.

--- Source/Core/Core/Core.cpp
+++ Source/Core/Core/Core.cpp
@@ -80,6 +80,7 @@
 
 #include "InputCommon/ControlReference/ControlReference.h"
 #include "InputCommon/ControllerInterface/ControllerInterface.h"
+#include "InputCommon/ControllerInterface/OctagonalMouseGate.h"
 #include "InputCommon/GCAdapter.h"
 
 #include "VideoCommon/AsyncRequests.h"
@@ -611,6 +612,9 @@ static void EmuThread(std::unique_ptr<BootParameters> boot, WindowSystemInfo wsi
   {
     PowerPC::SetMode(PowerPC::CoreMode::Interpreter);
   }
+
+  if (Config::Get(Config::MAIN_MOUSE_RECENTER_ON_BOOT))
+    g_controller_interface.SetMouseCenteringRequested(true);
 
   // ENTER THE VIDEO THREAD LOOP
   if (system.IsDualCoreMode())
